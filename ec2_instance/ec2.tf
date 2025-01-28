6f5177b4fb953f0749592bb239847bffcb794cd5
arun <arun@gmail.com>
2025-01-28 17:02:50 +0530

    create ec2 instance

---
+++ ec2_instance/ec2.tf
@@ -0,0 +1,36 @@
+# Declare the security group
+resource "aws_security_group" "terraform" {
+  name        = "terraform"
+  description = "Allow ssh"
+    tags = {
+    Name = "TERRAFORM"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   
+
+  # You can add more rules as needed
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0ac4dfaf1c5c0cce9"  # Replace with a valid AMI ID
+  instance_type = "t2.micro"
+   security_groups = [aws_security_group.terraform.name]
+
+  tags = {
+    Name = "ARUN"
+  }
+}
+
