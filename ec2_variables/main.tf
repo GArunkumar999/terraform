f7bf7aaf6b1686fafee89f77de28a3b4660396d2
arun <arun@gmail.com>
2025-01-31 15:48:45 +0530

    terraform

---
+++ ec2_variables/main.tf
@@ -0,0 +1,40 @@
+# Declare the security group
+resource "aws_security_group" "tf" {
+  name        = "tf"
+  description = "Allow ssh"
+tags = {
+  Name = "ALLOW-ALL"
+}
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  # You can add more rules as needed
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_instance" "newinstance" {
+  ami             = var.ami_id # Replace with a valid AMI ID
+  instance_type   = var.inst_type
+  key_name        = var.key_name
+  security_groups = [aws_security_group.tf.name]
+
+  tags = {
+    Name = "TERRAFORM"
+  }
+}
+
+
+
+
---
+++ ec2_variables/variable.tf
@@ -0,0 +1,17 @@
+variable "ami_id" {
+  type    = string
+  default = "ami-0ac4dfaf1c5c0cce9"
+}
+
+variable "inst_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  type    = string
+  default = "node"
+}
+
+
+
