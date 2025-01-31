753e21e6b8d472d033f0469f13df6eb429c24202
arun <arun@gmail.com>
2025-01-31 15:49:38 +0530

    terraform

---
+++ locals/locals.tf
@@ -0,0 +1,3 @@
+locals {
+  inst_type = var.environment == "prod" ? "t2.micro" : "t2.medium" 
+}
---
+++ locals/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "newinstance" {
+  ami           = "ami-0ac4dfaf1c5c0cce9"  # Replace with a valid AMI ID
+  instance_type = local.inst_type
+  key_name = "arun"
+
+  tags = {
+    Name = "TERRAFORM"
+  }
+}
